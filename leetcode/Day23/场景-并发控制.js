6750f23cd4551902296efa0ff2bdc2d5e0ccd790
zichen.xiang <857719037@qq.com>
2025-03-01 00:22:54 +0800

    day23 1/2

--- leetcode/Day23/场景-并发控制.js
+++ leetcode/Day23/场景-并发控制.js
@@ -1,4 +1,13 @@
 // TODO
-function limitQueue() {
-  
+// 模拟异步任务
+function run() {
+  setTimeout(() => {
+    
+  })
+}
+
+function limitQueue(tasks,limit) {
+  return new Promise((resolve, reject) => {
+    
+  })
 }
--- leetcode/Day23/算法-接雨水.js
+++ leetcode/Day23/算法-接雨水.js
@@ -1,3 +1,41 @@
 // TODO
 // 算法1：接雨水
 // 算法2: 盛最多水的容器
+
+
+// 算法1：接雨水
+function trap(height) {
+  const n = height.length
+  let leftMaxHeight = []
+  let rightMaxHeight = []
+  leftMaxHeight[0] = height[0]
+  rightMaxHeight[n - 1] = height[n - 1]
+  for (let i = 1; i < n; i++){
+    leftMaxHeight[i] = Math.max(leftMaxHeight[i - 1], height[i])
+    rightMaxHeight[n-i-1] = Math.max(rightMaxHeight[n-i],height[n-i-1])
+  }
+  let res = 0
+  for (let i = 1; i < n - 1; i++){
+    let value = Math.min(leftMaxHeight[i], rightMaxHeight[i]) - height[i]
+    res+=value
+  }
+  return res
+}
+
+
+// 算法2: 盛最多水的容器
+function maxArea(height) {
+  const n = height.length
+  let left = 0
+  let right = n - 1
+  let max = 0
+  while (left < right) {
+    max = Math.max(max, Math.min(height[left], height[right]) * (right - left))
+    if (height[left] >= height[right]) {
+      right -- 
+    } else {
+      left++
+    }
+  }
+  return max
+}
